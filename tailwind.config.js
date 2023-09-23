f41019634d0a4005bda717bc2fac37e6d879499f
Sabeel <sabeel.sabek@gmail.com>
2023-09-23 15:59:59 +0530

    Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors:{
+        'Neutral': {
+          50 : '#F9F9F9',
+          100: '#F4F4F4',
+          200: '#E4E4E4',
+          300: '#D3D3D3',
+          400: '#A2A2A2',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+          950: '#0A0A0A',
+          1000: '#000000',
+        },
+        'Twitterblue-default': '#1D9BF0',
+      },
+    },
   },
   plugins: [],
 }
